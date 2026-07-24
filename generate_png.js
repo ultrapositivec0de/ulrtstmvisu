6e0ed89f0cb80677e2fd53a80f92a065942eb59e
ultrapositivecode <testservisu@gmail.com>
2026-07-23 21:26:51 -0400

    fix

--- generate_png.js
+++ generate_png.js
@@ -1,11 +1,11 @@
-import { Jimp } from "jimp";
+import Jimp from "jimp";
 
 async function generateAppIcon() {
   // Create a new 512x512 image
-  const image = new Jimp({ width: 512, height: 512, color: 0x06b6d4ff }); // Cyan background
+  const image = await Jimp.create(512, 512, 0x06b6d4ff); // Cyan background
   
   // Save as app-icon.png
-  await image.write("app-icon.png");
+  await image.writeAsync("app-icon.png");
   console.log("Successfully created app-icon.png using Jimp");
 }
 
